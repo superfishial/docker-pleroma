9462b165933ab5a23f3b7a6fd7734dd2928f8051
Liam Dyer <liamcdyer@gmail.com>
2023-02-03 01:29:13 -0500

    fix: remove chown in start

--- start.sh
+++ start.sh
@@ -13,14 +13,11 @@ echo "
 Pleroma Docker by explodingcamera
 -------------------------------------
 -------------------------------------
-User:        $(whoami)    
+User:        $(whoami)
 User uid:    $(id -u pleroma)
 User gid:    $(id -g pleroma)
 -------------------------------------
 "
-
-chown -R pleroma:pleroma /app
-chown -R pleroma:pleroma /data
 
 echo "-- Waiting for database..."
 while ! pg_isready -U ${DB_USER:-pleroma} -d postgres://${DB_HOST:-db}:${DB_PORT:-5432}/${DB_NAME:-pleroma} -t 1; do
